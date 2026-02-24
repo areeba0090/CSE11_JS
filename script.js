2d3f64018ae16b978a8442a1d17220882092ec98
Areeba Alam <areeba.25b01010090@abes.ac.in>
2026-02-24 09:45:44 +0530

    Datatypes and JavaScript Data Values types

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-console.log("Hello World");
-document.write("hello world");
+// console.log("Hello World");
+// document.write("hello world");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Areeba";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+// function
+var sayHello=function() {
+    alert("hello world");
+}
+// array
+var numberArray=[1,2,3];
+var animals=new Array("cat","dog","mouse","lion")
+// object
+var person={
+    name:"Barack Obama II",
+    age:"51",
+    title:"44th President of the United States"
+}
+const sym1=Symbol(4)
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false")
+}
